6154d8e013ca02b5d403be38d807e875e393534d
Jack Diederich <jack@hivefire.com>
2012-08-17 16:40:24 -0400

    mostly working, fullscreen refresh is broke

--- straxe.py
+++ straxe.py
@@ -20,31 +20,6 @@ Things to colorize:
     gzip streams
     stats
     file read/write
-
-he following table lists the predefined colors:
-
-Constant	Color
-COLOR_BLACK	Black
-COLOR_BLUE	Blue
-COLOR_CYAN	Cyan (light greenish blue)
-COLOR_GREEN	Green
-COLOR_MAGENTA	Magenta (purplish red)
-COLOR_RED	Red
-COLOR_WHITE	White
-COLOR_YELLOW	Yellow
-
-Ways to group:
-  listen for a bit, find groups of operations
-
-Look into refresh args, it can do just a part of the screen.
-window.refresh([pminrow, pmincol, sminrow, smincol, smaxrow, smaxcol])
-Update the display immediately (sync actual screen with previous drawing/deleting methods).
-The 6 optional arguments can only be specified when the window is a pad created with newpad(). The additional parameters are needed to indicate what part of the pad and screen are involved. pminrow and pmincol specify the upper left-hand corner of the rectangle to be displayed in the pad. sminrow, smincol, smaxrow, and smaxcol specify the edges of the rectangle to be displayed on the screen. The lower right-hand corner of the rectangle to be displayed in the pad is calculated from the screen coordinates, since the rectangles must be the same size. Both rectangles must be entirely contained within their respective structures. Negative values of pminrow, pmincol, sminrow, or smincol are treated as if they were zero.
-
-or window.scroll() with many vertical windows (see subwin)
-
-window.subpad([nlines, ncols], begin_y, begin_x)
-Return a sub-window, whose upper-left corner is at (begin_y, begin_x), and whose width/height is ncols/nlines.
 '''
 
 def log(*args):
@@ -80,7 +55,7 @@ def get_lines(howlong=0.1):
         lines = []
         start = time.time()
         endtime = time.time() + howlong
-        while time.time() < endtime:
+        while not lines:
             try:
                 for c in sys.stdin.read(10):
                     curr += c
@@ -99,11 +74,10 @@ strace_re = re.compile('(\w+)\((\d*).*?(?:= (\d+).*)?$')
 def main(w):
     xx, yy = (curses.COLS - 1), (curses.LINES - 1)
     xx = 127
-    ww = w
     w = curses.newpad(yy, xx)
     buckets = [list(colorize_line(' ' * yy)) for _ in range(xx)]
     coloring = ['(?P<GREEN>SELECT)', '(?P<BLUE>(?:lstat|stat))', '^(?P<RED>writev)',
-                '"(?P<RED>HTTP/[^"]+)', '/data/code/(?P<CYAN>[^.]+\.\w+)',
+                '"(?P<RED>HTTP/[^"]+)', '/data/code/(?P<CYAN>[^.]+\.\w*)',
                 '(?P<WHITEB>GET[^"]+)"', '(?P<YELLOWB>portal_\w+_\w+_\w+)',
                ]
     for lines in get_lines():
@@ -116,17 +90,17 @@ def main(w):
             h %= len(buckets)
             if len(buckets[h]) < 1024 * 5: # skip backlogs
                 buckets[h] += list(colorize_line(line, coloring))
-        redraw(w, buckets, width=xx, height=yy-1, ww=ww)
+        redraw(w, buckets, width=xx, height=yy-1)
 
 
-def redraw(w, buckets, width, height, ww):
+def redraw(w, buckets, width, height):
     # slide and buckets that need moving
     do = []
     for x, buff in enumerate(buckets):
         if len(buff) >= height:
             buckets[x] = buff[1:]
             do.append((x, buff))
-    if False and len(do) > 0.3 * len(buckets):
+    if len(do) > 0.3 * len(buckets):
         redraw_fullscreen(w, buckets, width, height)
     else:
         redraw_incremental(w, do, width, height)
@@ -145,7 +119,7 @@ def print_this(w, cname, func, args):
     func(*args)
 
 def redraw_fullscreen(w, buckets, width, height):
-    for y in range(height):
+    for y in range(height-1):
         row = [bucket[y] for bucket in buckets]
         x = 0
         for color, pairs in it.groupby(row, lambda x:x[1]):
